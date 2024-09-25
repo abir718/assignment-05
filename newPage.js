b27fe70ac2642c171999e0c00fca88a85a99075c
abir718 <abir.hasan2003s@gmail.com>
2024-09-25 16:03:07 +0600

    history section added

--- newPage.js
+++ newPage.js
@@ -1,4 +1,3 @@
-
 function openPage(page) {
     window.location.href = page;
 }
@@ -15,12 +14,10 @@ let feniAmount = document.getElementById('feniDonation')
 let quotaAmount = document.getElementById('quotaDonation')
     quotaDonation = parseInt(quotaAmount.innerText)
 
-
 function inputValidation() {
     let inputNoakhali = document.getElementById('input')
     let input = inputNoakhali.value
     
-
     if (/^\d+$/.test(input) && parseInt(input) > 0) {
         let inputNumber = parseInt(input)
 
@@ -33,7 +30,8 @@ function inputValidation() {
             console.log(typeof availableCoins)
             document.getElementById("success-modal").checked = true; 
             inputNoakhali.value = ''
-                     
+            createDonationEntry(inputNumber , 'Flood relief in Noakhali , Bangladesh') 
+
         } else {
             alert("you dont have enough coins")
         }
@@ -42,10 +40,8 @@ function inputValidation() {
     else {
         alert("invalid amount")
     }
-   
-    
+       
 }
-
 
 function inputValidationFeni() {
     let inputFeni = document.getElementById('inputFeni')
@@ -62,7 +58,8 @@ function inputValidationFeni() {
             feniAmount.innerText = feniDonation
             document.getElementById("success-modal").checked = true; 
             inputFeni.value = ''
-                     
+            createDonationEntry(inputNumber , 'Flood relief in Feni , Bangladesh') 
+
         } else {
             alert("you dont have enough coins")
         }
@@ -71,14 +68,12 @@ function inputValidationFeni() {
     else {
         alert("invalid amount")
     }
-   
-    
+      
 }
 
 function inputValidationQuota() {
     let inputQuota = document.getElementById('inputQuota')
     input = inputQuota.value
-
 
     if (/^\d+$/.test(input) && parseInt(input) > 0) {
         let inputNumber = parseInt(input)
@@ -90,6 +85,8 @@ function inputValidationQuota() {
             quotaAmount.innerText = quotaDonation
             document.getElementById("success-modal").checked = true; 
             inputQuota.value = ''
+            createDonationEntry(inputNumber , 'quota movement of Bangladesh') 
+
                      
         } else {
             alert("you dont have enough coins")
@@ -100,9 +97,55 @@ function inputValidationQuota() {
         alert("invalid amount")
     }
    
-    
+   
 }
 
+function createDonationEntry(inputNumber , event) {
+
+    let now = new Date();
+    let dateString = now.toString(); 
+    let section = document.createElement('section')
+    section.classList.add('flex' , 'flex-col' , 'items-center')
+    let div = document.createElement('div');
+    div.classList.add('border', 'rounded-lg', 'p-4', 'mb-4', 'bg-white', 'w-[80%]');
+    div.innerHTML = `
+      <p class="font-bold text-lg mb-2">${inputNumber} Taka is Donated for the ${event}</p>
+      <p class="text-gray-500 text-sm">Date: ${dateString}</p>
+    `;
+    section.append(div)
+    document.getElementById('history').appendChild(section);
+    
+    }
+
+
+document.getElementById('historyButton').addEventListener('click' , function(){
+    let history = document.getElementById('historyButton')
+    history.classList.remove('border-2' , 'hover:bg-gray-200')
+    history.classList.add('bg-[#bef264]')
+
+    let donation = document.getElementById('donationButton')    
+    donation.classList.add('border-2' , 'hover:bg-gray-200')
+    donation.classList.remove('bg-[#bef264]')
+
+    document.getElementById('history').classList.remove('hidden')
+    document.getElementById('donation').classList.add('hidden')
+   
+})
+
+document.getElementById('donationButton').addEventListener('click' , function(){
+    let donation = document.getElementById('donationButton')
+    donation.classList.remove('border-2' , 'hover:bg-gray-200')
+    donation.classList.add('bg-[#bef264]')
+
+    let history = document.getElementById('historyButton')
+    history.classList.add('border-2' , 'hover:bg-gray-200')
+    history.classList.remove('bg-[#bef264]')
+
+
+    document.getElementById('donation').classList.remove('hidden')
+    document.getElementById('history').classList.add('hidden')
+   
+})
 
 
 
