f493fe5727e860680347c513dc873a6e4a14a759
abir718 <abir.hasan2003s@gmail.com>
2024-09-25 11:05:01 +0600

    noakhali donation calculation added

--- newPage.js
+++ newPage.js
@@ -3,6 +3,34 @@ function openPage(page) {
     window.location.href = page;
 }
 
+let totalCoins = document.getElementById('availableCoins')
+    availableCoins = parseInt(totalCoins.innerText)
 
+let noakhaliAmount = document.getElementById('noakhaliDonation')
+    noakhaliDonation = parseInt(noakhaliAmount.innerText)
+
+
+function inputValidation() {
+    let input = document.getElementById('input').value
+
+
+    if (/^\d+$/.test(input) && parseInt(input) > 0) {
+
+        if ( input <= availableCoins) {
+            availableCoins = availableCoins - input
+            totalCoins.innerText = availableCoins
+            document.getElementById("success-modal").checked = true; 
+                     
+        } else {
+            alert("you dont have enough coins")
+        }
+  
+    }  
+    else {
+        alert("invalid amount")
+    }
+   
+    
+}
 
 
